bded2111c4eb9f7fa7727230b95030bfd26e985c
RuiMSMelo <ruimsmelo@gmail.com>
2025-05-03 12:28:08 +0100

    added shader files

--- src/script.js
+++ src/script.js
@@ -21,30 +21,34 @@ const textureLoader = new THREE.TextureLoader()
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  25,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.set(1.5, 0, 6)
 scene.add(camera)
 
@@ -56,8 +60,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  antialias: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -66,24 +70,54 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Test
  */
 const test = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial()
 )
 scene.add(test)
 
 /**
+ * Fireworks
+ */
+const createFirework = (count, position) => {
+  // Geometry
+  const positionsArray = new Float32Array(count * 3)
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3
+
+    positionsArray[i3] = Math.random() - 0.5
+    positionsArray[i3 + 1] = Math.random() - 0.5
+    positionsArray[i3 + 2] = Math.random() - 0.5
+  }
+
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute(
+    'position',
+    new THREE.Float32BufferAttribute(positionsArray, 3)
+  )
+
+  // Material
+  const material = new THREE.PointsMaterial()
+
+  // Points
+  const firework = new THREE.Points(geometry, material)
+  firework.position.copy(position)
+  scene.add(firework)
+}
+createFirework(100, new THREE.Vector3())
+
+/**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+const tick = () => {
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
